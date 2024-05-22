f3e2c80a425e50b4d8de4c243f88949cb136cd59
savelievamareena <savelieva.mareena.1.21@gmail.com>
2024-05-22 18:47:10 +0300

    build: configure application

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ["plugin:@typescript-eslint/recommended-type-checked", "plugin:@typescript-eslint/stylistic-type-checked", "prettier"],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: [".eslintrc.js"],
+            parserOptions: {
+                sourceType: "script",
+            },
+        },
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: ['./tsconfig.json', './tsconfig.node.json'],
+        tsconfigRootDir: __dirname,
+    },
+    plugins: ["@typescript-eslint", "prettier"],
+    rules: {
+        "prettier/prettier": "error",
+        "react/prop-types": "off",
+        "react/react-in-jsx-scope": "off",
+    },
+};
