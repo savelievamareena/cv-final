d5d85db3d6ee2703da59ce8a30353f250af8cf9a
Andrey Klimkov <aaklimkov@gmail.com>
2024-05-24 17:43:31 +0300

    feat: add i18 setup

---
+++ src/i18n.ts
@@ -0,0 +1,15 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
+
+void i18n
+    .use(initReactI18next)
+    .use(LanguageDetector)
+    .use(Backend)
+    .init({
+        supportedLngs: ["en", "ru"],
+        fallbackLng: "en",
+    });
+
+export default i18n;
