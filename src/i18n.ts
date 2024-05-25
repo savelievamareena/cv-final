b4dc73f4fe367e30c6c4e46ef4d8aa4f3379e577
Andrey Klimkov <aaklimkov@gmail.com>
2024-05-25 10:39:07 +0300

    feat: add enum to language

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,13 +3,19 @@ import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import Backend from "i18next-http-backend";
 
+export enum Languages {
+    Ru = "ru",
+    En = "en",
+}
+
 void i18n
     .use(initReactI18next)
     .use(LanguageDetector)
     .use(Backend)
     .init({
-        supportedLngs: ["en", "ru"],
-        fallbackLng: "en",
+        supportedLngs: [Languages.En, Languages.Ru],
+
+        fallbackLng: Languages.En,
     });
 
 export default i18n;
