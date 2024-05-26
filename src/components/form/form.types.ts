b7db86b5785e5260d04c698f1360bbada2a24aec
savelievamareena <102456352+savelievamareena@users.noreply.github.com>
2024-05-26 17:20:41 +0300

    feat: add reusable Form and TextFormField components (#2)
    * feat: add reusable Form and TextFormField components
    
    * refactor: form Types
    
    * refactor: basic JS form substitution and adjusting TextFormField
    
    * refactor: rename form types
    
    * refactor: remove extensions in imports

---
+++ src/components/form/form.types.ts
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+import { DefaultValues, FieldValues, SubmitHandler } from "react-hook-form";
+import { FormProps as AntdFormProps } from "antd";
+import { ZodSchema } from "zod";
+
+export interface FormProps<T extends FieldValues> extends AntdFormProps {
+    schema: ZodSchema<T>;
+    onSubmit: SubmitHandler<T>;
+    children: ReactNode;
+    defaultValues: DefaultValues<T>;
+}
