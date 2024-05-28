57dfbaa9777175ab0e84950e57ba3fc90fb4ef62
Andrey Klimkov <aaklimkov@gmail.com>
2024-05-28 10:43:14 +0300

    fix: add  list layer to page

--- src/pages/Departments/Departments.page.tsx
+++ src/pages/Departments/Departments.page.tsx
@@ -1,34 +1,7 @@
-import { useState } from "react";
-import { Department } from "cv-graphql";
-import { t } from "i18next";
-import PageTemplate from "@/components/pageTemplate/page-template";
-import { Action } from "@/components/pageTemplate/actions-menu";
-import { ColumnConfig } from "@/components/pageTemplate/table-template";
-import { useDepartments } from "@/modules/departments/mutation/departments";
+import DepartmentsList from "@/modules/departments/ui/departments-list";
 
 const DepartmentsPage = () => {
-    const { departments, loading } = useDepartments();
-    const [searchQuery, setSearchQuery] = useState("");
-
-    const menuProps: Action = {
-        onDelete: () => console.log("delete"),
-        onUpdate: () => console.log("update"),
-    };
-
-    const columnConfigs: ColumnConfig<Department>[] = [{ name: "name", isSorted: true }];
-
-    return (
-        <PageTemplate
-            pageName={t("departent")}
-            onButtonClick={() => console.log(12)}
-            menuProps={menuProps}
-            columnConfigs={columnConfigs}
-            searchQuery={searchQuery}
-            displayData={departments}
-            loading={loading}
-            setSearchQuery={setSearchQuery}
-        />
-    );
+    return <DepartmentsList />;
 };
 
 export default DepartmentsPage;
---
+++ src/pages/Departments/index.ts
@@ -0,0 +1,3 @@
+import { lazy } from "react";
+
+export const DepartmentsPage = lazy(() => import("./Departments.page"));
