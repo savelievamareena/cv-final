95fbaf53868f0da9fabf0f199aeda68a9a734eb4
Andrey Klimkov <aaklimkov@gmail.com>
2024-05-28 10:31:51 +0300

    feat: add basic confirm dialog

--- src/pages/Departments/Departments.page.tsx
+++ src/pages/Departments/Departments.page.tsx
@@ -5,29 +5,42 @@ import PageTemplate from "@/components/pageTemplate/page-template";
 import { Action } from "@/components/pageTemplate/actions-menu";
 import { ColumnConfig } from "@/components/pageTemplate/table-template";
 import { useDepartments } from "@/modules/departments/mutation/departments";
+import useDialog from "@/helpers/create-dialog";
+import ConfirmDialog from "@/components/confirg-dialog/confirm-dialog";
 
 const DepartmentsPage = () => {
     const { departments, loading } = useDepartments();
     const [searchQuery, setSearchQuery] = useState("");
 
+    const { openDialog, DialogComponent } = useDialog({
+        dialog: ConfirmDialog,
+        dialogProps: {
+            title: t("Delete department?"),
+            onConfirm: () => console.log("delete"),
+        },
+    });
+
     const menuProps: Action = {
-        onDelete: () => console.log("delete"),
+        onDelete: openDialog,
         onUpdate: () => console.log("update"),
     };
 
     const columnConfigs: ColumnConfig<Department>[] = [{ name: "name", isSorted: true }];
 
     return (
-        <PageTemplate
-            pageName={t("departent")}
-            onButtonClick={() => console.log(12)}
-            menuProps={menuProps}
-            columnConfigs={columnConfigs}
-            searchQuery={searchQuery}
-            displayData={departments}
-            loading={loading}
-            setSearchQuery={setSearchQuery}
-        />
+        <>
+            <PageTemplate
+                pageName={t("departent")}
+                onButtonClick={() => console.log(12)}
+                menuProps={menuProps}
+                columnConfigs={columnConfigs}
+                searchQuery={searchQuery}
+                displayData={departments}
+                loading={loading}
+                setSearchQuery={setSearchQuery}
+            />
+            {DialogComponent}
+        </>
     );
 };
 
