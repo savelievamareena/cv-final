0a559fb3623d9d8e2226cf1a69abd8d2bca89184
Alexander <99599598+RC-3222@users.noreply.github.com>
2024-06-03 12:52:21 +0300

    refactor: tweak router configuration (#12)
    * refactor: tweak router configuration
    
    * fix: add error component

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -43,8 +43,9 @@ export const router = createBrowserRouter([
         errorElement: <ErrorComponent />,
         children: [
             {
+                errorElement: <ErrorComponent />,
                 index: true,
-                element: <Navigate to={routes.cvs.root} replace />,
+                element: <div>Users page content (table)</div>,
             },
             {
                 path: routes.cvs.root,
@@ -180,13 +181,18 @@ export const router = createBrowserRouter([
             },
             {
                 errorElement: <ErrorComponent />,
+                path: routes.positions,
+                element: <div>Positions page content (table)</div>,
+            },
+            {
+                errorElement: <ErrorComponent />,
                 path: routes.settings,
                 element: <div>Settings page content</div>,
             },
             {
                 index: true,
                 path: "*",
-                element: <Navigate to={routes.cvs.root} replace />,
+                element: <Navigate to='' replace />,
             },
         ],
     },
