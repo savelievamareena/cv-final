02a4470b2ef6477bdfe482cf0989982c049c7ee4
RC-3222 <tomgar9@outlook.com>
2024-06-11 12:58:03 +0300

    refactor: extract avatar upload handler

--- src/modules/users/components/avatar-upload/avatar-upload.tsx
+++ src/modules/users/components/avatar-upload/avatar-upload.tsx
@@ -1,5 +1,6 @@
 import { InboxOutlined } from "@ant-design/icons";
 import Dragger from "antd/es/upload/Dragger";
+import { RcFile } from "antd/es/upload";
 import { User } from "cv-graphql";
 import { useTranslation } from "react-i18next";
 import { fileToBase64 } from "@/helpers/file-to-base64";
@@ -13,32 +14,34 @@ const AvatarUpload = ({ user }: { user: User }) => {
 
     const { showNotification } = useNotificationContext();
 
+    const handleFileUpload = (file: RcFile) => {
+        if (!ALLOWED_IMAGE_TYPES.includes(file.type)) {
+            showNotification("error", t("profile.avatarUpload.errors.invalidFileType"));
+            return false;
+        }
+        if (file.size > MAX_AVATAR_SIZE) {
+            showNotification("error", t("profile.avatarUpload.errors.maxAvatarSize"));
+            return false;
+        }
+
+        fileToBase64(file)
+            .then((avatar) =>
+                uploadAvatar({ variables: { avatar: { userId: user.id, ...avatar } } })
+            )
+            .catch(() => {
+                showNotification("error", t("profile.avatarUpload.errors.generic"));
+            });
+
+        return false;
+    };
+
     return (
         <Dragger
             name="file"
             multiple={false}
             disabled={loading}
             showUploadList={false}
-            beforeUpload={(file) => {
-                if (!ALLOWED_IMAGE_TYPES.includes(file.type)) {
-                    showNotification("error", t("profile.avatarUpload.errors.invalidFileType"));
-                    return false;
-                }
-                if (file.size > MAX_AVATAR_SIZE) {
-                    showNotification("error", t("profile.avatarUpload.errors.maxAvatarSize"));
-                    return false;
-                }
-
-                fileToBase64(file)
-                    .then((avatar) =>
-                        uploadAvatar({ variables: { avatar: { userId: user.id, ...avatar } } })
-                    )
-                    .catch(() => {
-                        showNotification("error", t("profile.avatarUpload.errors.generic"));
-                    });
-
-                return false;
-            }}
+            beforeUpload={handleFileUpload}
         >
             <p className="ant-upload-drag-icon">
                 <InboxOutlined />
