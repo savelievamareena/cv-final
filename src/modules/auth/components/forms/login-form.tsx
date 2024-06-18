e9a20ab81d81665cd6bac973e72097ef0c973f10
RC-3222 <tomgar9@outlook.com>
2024-06-18 12:59:36 +0300

    refactor: make some adjustments to authentication logic

--- src/modules/auth/components/forms/login-form.tsx
+++ src/modules/auth/components/forms/login-form.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
 
 import { getLoginFormSchema } from "../../schemas";
 import { useLogin } from "../../api";
@@ -6,20 +7,30 @@ import { FormTextField } from "@/components/form-text-field";
 import { Form } from "@/components/form";
 import { PasswordInput } from "../password-input";
 import { FormSubmitButton } from "@/components/form-submit-button";
+import { useAuthUser } from "@/services/auth-service";
+import { useNotificationContext } from "@/helpers/notification";
+import { routes } from "@/router";
 
 import styles from "./form.module.scss";
 
 export const LoginForm = () => {
+    const user = useAuthUser();
+
     const [login, { loading }] = useLogin();
 
     const { t } = useTranslation();
+
+    const { showNotification } = useNotificationContext();
+
+    const isExistingUserNotVerified = user && !user.is_verified;
 
     return (
         <Form
             disabled={loading}
             className={styles.form}
             onSubmit={({ email, password }) => {
-                void login({ variables: { authData: { email, password } } });
+                if (isExistingUserNotVerified) showNotification("warning", t("verifyEmailWarning"));
+                else void login({ variables: { authData: { email, password } } });
             }}
             schema={getLoginFormSchema()}
         >
@@ -27,6 +38,11 @@ export const LoginForm = () => {
             <FormTextField type="text" label={t("auth.fieldLabels.email")} name="email" />
             <PasswordInput label={t("auth.fieldLabels.password")} name="password" />
             <FormSubmitButton>{t("submit")}</FormSubmitButton>
+            {isExistingUserNotVerified && (
+                <p>
+                    {t("verifyEmailHint")} <Link to={routes.auth.verification}>{t("here")}</Link>
+                </p>
+            )}
         </Form>
     );
 };
