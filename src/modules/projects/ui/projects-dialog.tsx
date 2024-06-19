2fc7ab48075aa092ebcbf70621292cf95503b498
Andrey Klimkov <aaklimkov@gmail.com>
2024-06-19 13:38:35 +0300

    feat: add translation

--- src/modules/projects/ui/projects-dialog.tsx
+++ src/modules/projects/ui/projects-dialog.tsx
@@ -1,11 +1,12 @@
 import { useTranslation } from "react-i18next";
-import { Button } from "antd";
+import { Button, DatePicker, Form as AntdForm } from "antd";
 import { Form } from "@/components/form";
 import { FormTextField } from "@/components/form-text-field";
 import { createDialogHook } from "@/helpers/dialog/create-dialog";
 import { BaseDialog } from "@/components/base-dialog/";
 import { FormSubmitButton } from "@/components/form-submit-button";
 import { getProjectFormSchema, ProjectFormSchemaType } from "../shemas/project";
+import { Controller } from "react-hook-form";
 
 interface ProjectDialogProps {
     title: string;
@@ -29,13 +30,37 @@ const ProjectDialog = ({ title, onConfirm, onClose, initialValues }: ProjectDial
                 defaultValues={initialValues}
                 schema={getProjectFormSchema()}
             >
-                <FormTextField type="text" label={t("projects.projects")} name="name" />
-                <FormTextField type="text" label={t("projects.projects")} name="internalName" />
-                <FormTextField type="text" label={t("projects.projects")} name="domain" />
-                <FormTextField type="text" label={t("projects.projects")} name="description" />
-                <FormTextField type="text" label={t("projects.projects")} name="startDate" />
-                <FormTextField type="text" label={t("projects.projects")} name="endDate" />
-                <FormTextField type="number" label={t("projects.projects")} name="teamSize" />
+                <FormTextField type="text" label={t("projects.name")} name="name" />
+                <FormTextField type="text" label={t("projects.internalName")} name="internalName" />
+                <FormTextField type="text" label={t("projects.domain")} name="domain" />
+                <FormTextField type="text" label={t("projects.description")} name="description" />
+                <AntdForm.Item label={t("projects.startDate")}>
+                    <Controller
+                        name="startDate"
+                        render={({ field }) => (
+                            <DatePicker
+                                {...field}
+                                format="YYYY-MM-DD"
+                                placeholder={t("projects.startDate")}
+                                onChange={(dateString) => field.onChange(dateString)}
+                            />
+                        )}
+                    />
+                </AntdForm.Item>
+                <AntdForm.Item label={t("projects.endDate")}>
+                    <Controller
+                        name="endDate"
+                        render={({ field }) => (
+                            <DatePicker
+                                {...field}
+                                format="YYYY-MM-DD"
+                                placeholder={t("projects.endDate")}
+                                onChange={(dateString) => field.onChange(dateString)}
+                            />
+                        )}
+                    />
+                </AntdForm.Item>
+                <FormTextField type="number" label={t("projects.teamSize")} name="teamSize" />
                 <Button htmlType="button" onClick={onClose}>
                     {t("cancel")}
                 </Button>
