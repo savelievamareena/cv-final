7f6f6c1426d42b5d4f2a40465e70ae2ea925debd
Andrey Klimkov <aaklimkov@gmail.com>
2024-06-25 13:36:07 +0300

    fix: remove unnessesary files

--- src/modules/cvs/api/index.ts
+++ src/modules/cvs/api/index.ts
@@ -2,4 +2,4 @@ export { useCvCreate } from "./create-cv-mutation.ts";
 export { useCvDelete } from "./delete-cv-mutation.ts";
 export { useCvUpdate } from "./update-cv-mutation.ts";
 export { useCvsQuery } from "./get-cvs-query.ts";
-export { useCvById } from "./get-cv-by-id-query.ts";
+export { useCvById } from "./get-cv-by-id-query";
